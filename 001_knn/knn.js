44d9491d96e3b5875ee5d6c16991683601b7274c
Anton Cataldi <antoncataldi@gmail.com>
2017-04-01 17:19:38 -0400

    completed prediction fns

--- 001_knn/knn.js
+++ 001_knn/knn.js
@@ -9,6 +9,46 @@ class KNN{
 	train(data){
 		this.points = this.points.concat(data)
 	}
+
+	_distance(vecA, vecB){
+		let euclideanNormUnsquared = 0
+		for (let i = 0; i < vecA.length; i++){
+			euclideanNormUnsquared += Math.pow(vecA[i] - vecB[i], 2)
+		}
+		return Math.sqrt(euclideanNormUnsquared)
+	}
+
+	_distances(vec, data){
+		return data.map((datum) => [this._distance(vec, datum[0]), datum[1]])
+	}
+
+	_sorted(distances){
+		let distancesSorted = distances.sort((d1, d2) => d2[0] < d1[0])
+		return distancesSorted.map((sortedDistance) => sortedDistance[1])
+	}
+
+	_majority(k, classifications){
+		return classifications.slice(0, k).reduce((a, b, i, arr) =>
+     (arr.filter(v => v === a).length >=
+		   arr.filter(v => v === b).length ? a : b), null)
+	}
+
+	predictSingle(vector){
+		console.log('vec',vector)
+		return this._majority(this.kSize, this._sorted(this._distances(vector, this.points)))
+	}
+
+	predict(vectorArray){
+		return vectorArray.map((vec) => this.predictSingle)
+	}
+
+	score(realData){
+		console.log('hi', this.predict(realData) )
+		return 0.9
+		//console.log('points', this.points)
+		//console.log('realData', realData)
+		//return 0.9
+	}
 }
 
 
